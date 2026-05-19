19e1353870b5901704d405ff3bdf5100423c26eb
A Student <student@nowhere.edu>
2026-05-19 05:49:36 +0000

    May 18, 2026, 10:49 PM

--- tests.cpp
+++ tests.cpp
@@ -1,53 +1,256 @@
+//----------------------------------------------------------
+// CS162 Assignment Starter Code
+// Copyright Andrew Scholer (ascholer@chemeketa.edu)
+// Neither this code, nor any works derived from it
+//    may be republished without approval.
+//----------------------------------------------------------
+
+// Bring in unit testing code and tell it to build a main function
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest.h"
 
+// Use Approx from doctest without saying doctest::Approx
 using doctest::Approx;
 
-int squareDifference(int a, int b)
+//-----------------------------------------------------------------------------------
+
+#include "GrayscaleImage.h"
+
+using namespace std;
+
+// Helpers:
+
+void wipe(GrayscaleImage& img)
 {
-  return (a - b) * (a - b);
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+      img.setPixel(i, j, 0);
+    }
+  }
+}
+void getGradientFill(GrayscaleImage& img)
+{
+  uint8_t data[3][5] = {
+      {0, 63, 127, 191, 255}, {0, 63, 127, 191, 255}, {0, 63, 127, 191, 255}};
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+      img.setPixel(i, j, data[i][j]);
+    }
+  }
 }
 
-double distance(int x1, int y1, int x2, int y2)
+// Tests:
+TEST_CASE("constructor")
 {
-  return sqrt(squareDifference(x1, x2) + squareDifference(y1, y2));
+  cout << "1: Basic Contructor" << endl;
+  GrayscaleImage g(3, 5);
+
+  REQUIRE(g.getWidth() == 5);
+  REQUIRE(g.getHeight() == 3);
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+      REQUIRE(g.getPixel(i, j) == 0);
+    }
+  }
 }
 
-TEST_CASE("squareDifference")
+TEST_CASE("setPixel")
 {
-  // if a CHECK fails, the test case as a whole will fail, but still
-  // try the rest of the assertions.
-  CHECK(squareDifference(1, 4) == 9);
+  cout << "2: setPixel" << endl;
+  GrayscaleImage g(3, 5);
 
-  // if a REQUIRE fails, the rest of the assertions in this
-  // test case aren't even worth trying---just move on to the next
-  // test case.
-  REQUIRE(squareDifference(1, 4) == 9);
+  REQUIRE(g.getWidth() == 5);
+  REQUIRE(g.getHeight() == 3);
 
-  // Test that ordering does not matter
-  CHECK(squareDifference(4, 1) == 9);
+  // Test setting a pixel
+  g.setPixel(1, 2, 100);
+  REQUIRE(g.getPixel(1, 2) == 100);
 
-  // Test that negatives are handled
-  int answer3 = squareDifference(-2, -6);
-  CHECK(answer3 == 16);
+  // Test that other pixels are unchanged
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (i == 1 && j == 2)
+        continue;
+      REQUIRE(g.getPixel(i, j) == 0);
+    }
+  }
 }
 
-TEST_CASE("distance")
+TEST_CASE("fill")
 {
-  int x1 = 0, y1 = 0;
-  int x2 = 3, y2 = 4;
-  int x3 = -2, y3 = -2;
+  cout << "3: fill" << endl;
+  GrayscaleImage g(10, 8);
 
-  // Approx sees if a double value is "close enough" to believe that any
-  // difference is based on rounding error.  Always test doubles using
-  // Approx.  It only exists in unit tests---do not use in normal code.
-  CHECK(distance(x1, y1, x2, y2) == Approx(5.0));
+  g.fill(101);
 
-  // Check reverse direction
-  CHECK(distance(x2, y2, x1, y1) == Approx(5.0));
+  for (int i = 0; i < g.getHeight(); i++) {
+    for (int j = 0; j < g.getWidth(); j++) {
+      REQUIRE(g.getPixel(i, j) == 101);
+    }
+  }
+}
 
-  // Test some negative coordinate values
-  double answer2 = distance(x1, y1, x3, y3);
-  double answer2Desired = 2 * sqrt(2);
-  CHECK(answer2 == Approx(answer2Desired));
+TEST_CASE("toString")
+{
+  cout << "4: toString" << endl;
+  GrayscaleImage g(2, 3);
+  g.setPixel(0, 1, 100);
+  g.setPixel(1, 0, 25);
+  g.setPixel(1, 2, 50);
+
+  string produced = g.toString();
+
+  string desired = "0\t100\t0\t\n25\t0\t50\t\n";
+
+  REQUIRE(produced == desired);
+}
+
+TEST_CASE("operator==")
+{
+  cout << "5: Operator ==" << endl;
+  GrayscaleImage g(3, 5);
+  GrayscaleImage g2(3, 5);
+  GrayscaleImage g3(2, 2);
+
+  CHECK(g == g2);
+  CHECK(!(g == g3));
+
+  // Try changing pixels and make sure == behaves correctly
+  g2.setPixel(2, 4, 100);
+  CHECK(!(g == g2));
+
+  g.setPixel(2, 4, 100);
+  CHECK(g == g2);
+
+  g2.setPixel(1, 0, 100);
+  CHECK(!(g == g2));
+}
+
+TEST_CASE("copy constuctor")
+{
+  cout << "6: Copy ctor" << endl;
+  GrayscaleImage g(3, 5);
+  getGradientFill(g); // Add some stuff to it
+
+  GrayscaleImage g2(g);
+  REQUIRE(g == g2);
+
+  // Make sure we have a deep copy
+  wipe(g);
+
+  GrayscaleImage gradient(3, 5);
+  getGradientFill(gradient);
+  REQUIRE(g2 == gradient);
+}
+
+TEST_CASE("assignment")
+{
+  cout << "7: Assignment operator" << endl;
+  GrayscaleImage g(3, 5);
+  getGradientFill(g); // Add some stuff to it
+
+  GrayscaleImage g2(1, 1);
+  g2 = g;
+  REQUIRE(g == g2);
+
+  // Make sure we have a deep copy
+  wipe(g);
+  GrayscaleImage gradient(3, 5);
+  getGradientFill(gradient);
+  REQUIRE(g2 == gradient);
+}
+
+TEST_CASE("addFrame")
+{
+  cout << "8: addFrame" << endl;
+  GrayscaleImage g(3, 5);
+  getGradientFill(g); // Add some stuff to it
+
+  // Make a version with a 1 px border with brightness 111
+  GrayscaleImage g2 = g.addFrame(1, 111);
+
+  // These will display if you fail this test
+  INFO("g original:\n", g.toString());
+  INFO("g2 (frame):\n", g2.toString());
+
+  REQUIRE(g2.getHeight() == 5);
+  REQUIRE(g2.getWidth() == 7);
+  // Test a few pixels
+  REQUIRE(g2.getPixel(0, 2) == 111);
+  REQUIRE(g2.getPixel(1, 0) == 111);
+  REQUIRE(g2.getPixel(2, 6) == 111);
+  REQUIRE(g2.getPixel(4, 3) == 111);
+  REQUIRE(g2.getPixel(3, 3) == 127);
+
+  // Make a version with 2 px black border around the 1px 111 border
+  GrayscaleImage g3 = g2.addFrame(2, 0);
+
+  // These will display if you fail this test
+  INFO("g2:\n", g2.toString());
+  INFO("g3:\n", g3.toString());
+
+  REQUIRE(g3.getHeight() == 9);
+  REQUIRE(g3.getWidth() == 11);
+  // Test a few pixels
+  REQUIRE(g3.getPixel(0, 1) == 0);
+  REQUIRE(g3.getPixel(2, 4) == 111);
+  REQUIRE(g3.getPixel(4, 4) == 63);
+}
+
+TEST_CASE("crop")
+{
+  cout << "9a: crop" << endl;
+  GrayscaleImage g(3, 5);
+  getGradientFill(g); // Add some stuff to it
+
+  GrayscaleImage g2 = g.crop(0, 0, 2, 3);
+
+  // These will display if you fail this test
+  INFO("g original:\n", g.toString());
+  INFO("g2 (crop) :\n", g2.toString());
+
+  REQUIRE(g2.getHeight() == 2);
+  REQUIRE(g2.getWidth() == 3);
+
+  REQUIRE(g2.getPixel(0, 0) == 0);
+  REQUIRE(g2.getPixel(1, 1) == 63);
+  REQUIRE(g2.getPixel(1, 2) == 127);
+
+  GrayscaleImage g3 = g.crop(1, 2, 2, 2);
+
+  // These will display if you fail this test
+  INFO("g original:\n", g.toString());
+  INFO("g3 (crop) :\n", g3.toString());
+
+  REQUIRE(g3.getHeight() == 2);
+  REQUIRE(g3.getWidth() == 2);
+
+  REQUIRE(g3.getPixel(0, 0) == 127);
+  REQUIRE(g3.getPixel(1, 1) == 191);
+}
+
+TEST_CASE("invalid crop")
+{
+  cout << "9b: invalid crop" << endl;
+  GrayscaleImage g(3, 5);
+
+  bool caught = false;
+
+  try {
+    GrayscaleImage g3 = g.crop(1, -1, 2, 2);
+  } catch (out_of_range& e) {
+    caught = true;
+  }
+  REQUIRE(caught);
+
+  caught = false;
+  try {
+    GrayscaleImage g3 = g.crop(1, 1, 6, 2);
+  } catch (out_of_range& e) {
+    caught = true;
+  }
+  REQUIRE(caught);
 }
