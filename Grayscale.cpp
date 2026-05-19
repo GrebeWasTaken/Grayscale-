7cbd8ec43ce005915024a0821b2d49fb680aae03
A Student <student@nowhere.edu>
2026-05-19 05:53:57 +0000

    May 18, 2026, 10:53 PM

---
+++ Grayscale.cpp
@@ -0,0 +1,27 @@
+//----------------------------------------------------------
+// CS162 Assignment Starter Code
+// Copyright Andrew Scholer (ascholer@chemeketa.edu)
+// Neither this code, nor any works derived from it
+//    may be republished without approval.
+//----------------------------------------------------------
+
+#include <stdexcept>
+#include <string>
+using namespace std;
+
+#include "GrayscaleImage.h"
+
+
+int GrayscaleImage::getWidth() const {
+    return m_width;
+}
+
+int GrayscaleImage::getHeight() const {
+    return m_height;
+}
+
+uint8_t GrayscaleImage::getPixel(int row, int col) const {
+    if(row >= m_height || row < 0 || col >= m_width || col < 0)
+        throw out_of_range("Bad index in getPixel");
+    return m_pixels[row * m_width + col];
+}
