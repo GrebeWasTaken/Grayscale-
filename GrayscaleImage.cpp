41a7ef8afb3047957d35a8a0ea1afc4901d2eae1
A Student <student@nowhere.edu>
2026-05-20 05:16:44 +0000

    May 19, 2026, 10:16 PM

--- GrayscaleImage.cpp
+++ GrayscaleImage.cpp
@@ -39,5 +39,5 @@ uint8_t GrayscaleImage::getPixel(int row, int col) const
 void GrayscaleImage::setPixel(int row, int col, uint8_t brightness){
   m_pixels[row * m_width + col] = brightness;
 
-  if()
+  if(row >= m_height || row < 0 || )
 }
