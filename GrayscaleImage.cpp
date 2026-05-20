90865192e71ad90f3156ffecf3678f0280ac8cda
A Student <student@nowhere.edu>
2026-05-20 05:37:08 +0000

    May 19, 2026, 10:37 PM

--- GrayscaleImage.cpp
+++ GrayscaleImage.cpp
@@ -72,6 +72,8 @@ string GrayscaleImage::toString() const{
 }
 
 bool GrayscaleImage::operator==(const GrayscaleImage& other) const{
-  if(m_height != other.m_height || m_width )
+  if(m_height != other.m_height || m_width != other.m_width){
+    return 
+  }
 }
 
