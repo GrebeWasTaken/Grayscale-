6b6775899e001ccf8d284d65d5e1e77915fe7eea
A Student <student@nowhere.edu>
2026-05-20 05:51:58 +0000

    May 19, 2026, 10:51 PM

--- GrayscaleImage.cpp
+++ GrayscaleImage.cpp
@@ -102,5 +102,5 @@ GrayscaleImage GrayscaleImage::addFrame(int padding, uint8_t brightness) const{
 }
 
 GrayscaleImage GrayscaleImage::crop(int startRow, int startCol, int newHeight, int newWidth) const{
-  if(startRow < 0 || startCol < 0 || newHeight < 0 || newWidth < 0 || startRow + newHeight > m_height || startCol +)
+  if(startRow < 0 || startCol < 0 || newHeight < 0 || newWidth < 0 || startRow + newHeight > m_height || startCol + newWidth > m_width)
 }
