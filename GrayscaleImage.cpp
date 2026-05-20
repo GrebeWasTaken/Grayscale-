2a02a1155e06acdf14bf359f78900cad3c3f5c70
A Student <student@nowhere.edu>
2026-05-20 05:46:01 +0000

    May 19, 2026, 10:46 PM

--- GrayscaleImage.cpp
+++ GrayscaleImage.cpp
@@ -87,6 +87,6 @@ bool GrayscaleImage::operator==(const GrayscaleImage& other) const{
 
 GrayscaleImage GrayscaleImage::addFrame(int padding, uint8_t brightness) const{
   int newHeight = m_height + padding * 2;
-  
+  int newWidth = m_
 }
 
